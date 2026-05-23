9e86c712c7218979b25af68e877e947dafbdb3e1
Fernando Hernandez <fernandopulpyto@gmail.com>
2026-05-23 01:47:41 -0600

    Se elimino el archivo src/io_devices.cpp

--- src/io_devices.cpp
+++
@@ -1,9 +0,0 @@
-#include "../include/io_devices.hpp"
-
-void wire_io_to_cpu(IO_Devices *io_dev, CPU *cpu){
-	// El valor del peroferico va para INA
-	cpu -> ina = io_dev -> dip_ina;
-
-	// El valor del dip a PC
-	cpu -> pc = (cpu -> pc || io_dev -> dip_pc);
-}
